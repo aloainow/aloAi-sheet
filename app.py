1458b7b5941af11ee496385001b928a7a3ab1f8e
aloainow <149620618+aloainow@users.noreply.github.com>
2023-11-11 15:58:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ from sklearn.linear_model import LinearRegression
 
 
 #api_key1 = st.secrets["GOOGLE_API_KEY"]
-api_key = st.secrets["OPENAI_API_KEY"]
-#os.environ["GOOGLE_API_KEY"] ="AIzaSyD29fEos3V6S2L-AGSQgNu03GqZEIgJads"
-os.environ["OPENAI_API_KEY"] = api_key
+#api_key = st.secrets["OPENAI_API_KEY"]
+os.environ["GOOGLE_API_KEY"] ="AIzaSyD29fEos3V6S2L-AGSQgNu03GqZEIgJads"
+#os.environ["OPENAI_API_KEY"] = api_key
 
 
 
