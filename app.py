ba8996d0c21b1271e58fdac8170e9793f59cf289
aloainow <149620618+aloainow@users.noreply.github.com>
2023-11-24 18:31:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from sklearn.linear_model import LinearRegression
 api_key = st.secrets["OPENAI_API_KEY"]
 os.environ["GOOGLE_API_KEY"] ="AIzaSyD29fEos3V6S2L-AGSQgNu03GqZEIgJads"
 os.environ["OPENAI_API_KEY"] = api_key
-llm1 = ChatOpenAI(temperature=0.5,max_tokens= 2048)  
+llm1 = ChatOpenAI(temperature=0.5)  
 llm2 = GooglePalm(temperature=0.5, max_output_tokens= 2048,verbose=True,streaming=True)
 
 
